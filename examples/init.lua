2e7969c7df31c2b9af5e1c202ad276de19b5cec1
landoyjx <landoyjx@163.com>
2019-11-22 19:15:59 +0800

    update luac

--- examples/init.lua
+++ examples/init.lua
@@ -1,13 +1,17 @@
 function check()
+  syscall_print("do check")
   return 0
 end
 
 function main(args)
+  syscall_print("main1")
   contract["hello"]="world"
+  syscall_print("main2")
   return 0
 end
 
 function init(args)
+  syscall_print("init")
   contract = {}
   for i=1,#(args) do
 	contract["arg"..i]=args[i]
